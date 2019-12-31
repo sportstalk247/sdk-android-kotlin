60c83d5b62f4bdd5388ced0d445aa59e5266066d
unnivm <unnivm@gmail.com>
2019-12-31 15:29:06 +0530

    corrected test package name

--- app/src/androidTest/java/com/sportstalk/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sportstalk/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.binu.myapplication;
+package com.sportstalk;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.binu.myapplication", appContext.getPackageName());
+        assertEquals("com.sportstalk", appContext.getPackageName());
     }
 }
