d17447ce5e6a63144980bfcaba824d17dcbcc135
Lawrence C. Cendaña <lawrencec@towa.network>
2021-02-01 18:42:45 +0800

    Add role and customtags field under User datamodel

--- datamodels/src/main/java/com/sportstalk/datamodels/users/User.kt
+++ datamodels/src/main/java/com/sportstalk/datamodels/users/User.kt
@@ -14,12 +14,23 @@ data class User(
         val displayname: String? = null,
         val pictureurl: String? = null,
         val profileurl: String? = null,
+        /**
+         * [Role]
+         */
+        val role: String? = null,
+        val customtags: List<String> = listOf(),
         val banned: Boolean? = null,
         val shadowbanned: Boolean? = null,
         val shadowbanexpires: String? = null,   // ISODate ex. "2020-11-11T11:35:07.657812Z"
         val moderation: String? = null,   // "unknown"
         val reports: List<UserReport> = listOf()
-): Parcelable
+): Parcelable {
+        object Role {
+                const val USER = "user"
+                const val MODERATOR = "moderator"
+                const val ADMIN = "admin"
+        }
+}
 
 @Parcelize
 @Serializable
