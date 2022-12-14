d8e2100052f907a8f83eb4eac39fa6a2c8371436
Lawrence C. Cendaña <lawrence.cendana@rsvp.com.au>
2022-12-15 01:27:36 +0800

    Update implementation of Chat Client - Create Room

--- datamodels/src/main/java/com/sportstalk/datamodels/chat/CreateChatRoomRequest.kt
+++ datamodels/src/main/java/com/sportstalk/datamodels/chat/CreateChatRoomRequest.kt
@@ -1,5 +1,6 @@
 package com.sportstalk.datamodels.chat
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -15,5 +16,12 @@ data class CreateChatRoomRequest(
         val enableautoexpiresessions: Boolean? = null,
         val delaymessageseconds: Long? = null,
         val roomisopen: Boolean? = null,
-        val maxreports: Long? = null
+        val maxreports: Long? = null,
+        val customtype: String? = null, // (optional) Custom type string
+        val custompayload: String? = null,  // (optional) Custom payload string.
+        val customfield1: String? = null,   // (optional) User custom field 1. Store any string value you want here, limit 1024 bytes.
+        val customfield2: String? = null, // (optional) User custom field 2. Store any string value you want here, limit 1024 bytes.
+        val customtags: List<String>? = null,   // (optional) A comma delimited list of tags
+        @SerialName("private")
+        val `private`: Boolean? = null  // (false | optional) If set to true a user cannot join this room unless explicitly granted with a call to the Set User Permission API.
 )
