39448632b68dc24a7a3963e4f470d28117d508da
Lawrence C. Cendaña <lawrence.cendana@gmail.com>
2024-08-13 15:56:17 +0800

    Fix readthedocs build error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath(os.getenv("PYTHON_SITE_PACKAGES")))
+# Uncomment this part if building on local machine
+# sys.path.insert(0, os.path.abspath(os.getenv("PYTHON_SITE_PACKAGES")))
 import recommonmark
 from recommonmark.transform import AutoStructify
 import sphinx_tabs
